ef57e682d3e339dc974269c5396d894ab7550f01
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-18 16:47:59 +0600

    localhost created

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+
+
+// middleware 
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+
+
+
+
+
+
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`listening on ${port}`);
+})
