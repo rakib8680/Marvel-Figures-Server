b466386f5e3c32f6e333694127e00d8cc6ce5cd2
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-19 18:49:33 +0600

    single toy find added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -39,16 +39,39 @@ async function run() {
         const toysCollection = client.db('toysDB').collection('toys');
 
         // post to toysDB
-        app.post('/allToys', async(req, res) => {
+        app.post('/allToys', async (req, res) => {
             const newToy = req.body
             const result = await toysCollection.insertOne(newToy);
             res.send(result);
         })
 
         // get from toyDB 
-        app.get('/allToys', async(req, res) => {
+        app.get('/allToys', async (req, res) => {
             const result = await toysCollection.find().toArray()
             res.send(result)
+        });
+
+
+        // get single toy 
+        app.get('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        })
+
+        // delete toy 
+        app.delete('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId }
+            const result = await toysCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // patch toys 
+        app.patch('/allToys/:id', async (req, res) => {
+            const id = req.params.id
+
         })
 
 
